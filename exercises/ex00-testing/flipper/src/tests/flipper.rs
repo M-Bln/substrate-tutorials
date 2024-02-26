e2db3105669101ea3511dd82114a14551e679917
M-Bln <mlnd2@protonmail.com>
2024-02-26 15:19:57 +0100

    ex00 solved

--- exercises/ex00-testing/flipper/src/tests/flipper.rs
+++ exercises/ex00-testing/flipper/src/tests/flipper.rs
@@ -29,12 +29,26 @@ fn set_value_err_already_set() {
 
 #[test]
 fn flip_value_ok() {
-	new_test_ext()
-		.execute_with(|| todo!("Ensure the good behaviour of the flip_value() function."));
+	new_test_ext().execute_with(|| {
+		assert_ok!(Flipper::set_value(Origin::signed(ALICE), false));
+		assert_eq!(Flipper::value(), Some(false));
+		assert_ok!(Flipper::flip_value(Origin::signed(ALICE)));
+		assert_eq!(Flipper::value(), Some(true));
+	});
+	//.execute_with(|| todo!("Ensure the good behaviour of the flip_value() function."));
 }
 
 #[test]
 fn flip_value_ko() {
-	new_test_ext()
-		.execute_with(|| todo!("write a scenario that triggers an error in flip_value()"));
+	new_test_ext().execute_with(|| {
+		assert_eq!(
+			Flipper::flip_value(Origin::signed(ALICE)),
+			Err(Module(ModuleError {
+				index: 1,
+				error: [0, 0, 0, 0,],
+				message: Some("NoneValue",),
+			},),)
+		);
+	});
+	//.execute_with(|| todo!("write a scenario that triggers an error in flip_value()"));
 }
