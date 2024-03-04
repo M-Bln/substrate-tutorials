e8405f63386e3850bc4b0afeedfe68e9eb523b92
M-Bln <mlnd2@protonmail.com>
2024-03-04 22:26:58 +0100

    ex07 solved

--- exercises/ex07-imbalances/imbalances/src/lib.rs
+++ exercises/ex07-imbalances/imbalances/src/lib.rs
@@ -9,7 +9,7 @@ use sp_std::vec::Vec;
 
 use frame_support::{
 	sp_runtime::traits::{CheckedConversion, CheckedMul},
-	traits::{Currency, Imbalance, TryDrop},
+	traits::{Currency, Imbalance, SignedImbalance, TryDrop},
 	transactional,
 };
 
@@ -68,11 +68,23 @@ pub mod pallet {
 			// If we do nothing more, those tokens will be removed when the `NegativeImbalance`
 			// contained in the `amount_to_distribute` variable will be drop
 			let amount_to_distribute = T::Currency::issue(amount);
+			match T::Currency::deposit_into_existing(&beneficiary, amount_to_distribute.peek()) {
+				Ok(_) => Ok(()),
+				Err(_) => Err(DispatchError::Module(
+					frame_support::sp_runtime::ModuleError {
+						index: 2,
+						error: [0, 0, 0, 0],
+						message: Some("AccountDoesNotExist"),
+					},
+				)),
+			}
+			//amount_to_distribute.offset(amount_distributed);
+			//amount_to_distribute.drop_zero();
 			// TODO
 			// We want to compensate this imbalance by increasing `benefeciary` balance by the
 			// corresponding amount
 
-			Ok(())
+			//			Ok(())
 		}
 
 		#[pallet::weight(0)]
@@ -84,10 +96,18 @@ pub mod pallet {
 			ensure_root(origin)?;
 
 			// Todo: slash target
+			let (amount_slashed, _) = T::Currency::slash(&target, amount);
+
 			// Todo: give 1/3 of the slashed amount to the treasury and burn the rest
 			// Hint: use the `ration` method
 			// Hint: TreasuryAccount is defined as on l35 as a Config constant
-
+			let (to_treasury, to_burn) = amount_slashed.ration(1, 2);
+			T::Currency::burn(to_burn.peek());
+			T::Currency::deposit_creating(&T::TreasuryAccount::get(), to_treasury.peek());
+			// match T::Currency::deposit_into_existing(&T::TreasuryAccount::get(), to_treasury.peek()) {
+			// 	Ok(_) => Ok(()),
+			// 	Err(_) => Err(DispatchError::Module(frame_support::sp_runtime::ModuleError { index: 2, error: [0, 0, 0, 0], message: Some("AccountDoesNotExist") })),
+			// }
 			Ok(())
 		}
 
@@ -99,15 +119,60 @@ pub mod pallet {
 			beneficiary: T::AccountId,
 		) -> DispatchResult {
 			ensure_root(origin)?;
+			let mut to_beneficiary =
+				<T::Currency as Currency<T::AccountId>>::NegativeImbalance::zero();
+			let mut to_treasury =
+				<T::Currency as Currency<T::AccountId>>::NegativeImbalance::zero();
+			for sacked_account in sacked_accounts {
+				match T::Currency::make_free_balance_be(
+					&sacked_account,
+					T::Currency::minimum_balance(),
+				) {
+					SignedImbalance::Negative(amount_slashed) => {
+						let (treasury_cut, beneficiary_cut) =
+							amount_slashed.split(T::TreasuryFlatCut::get());
+						to_beneficiary = to_beneficiary.merge(beneficiary_cut);
+						to_treasury = to_treasury.merge(treasury_cut);
+					},
+					_ => {
+						return Err(DispatchError::Module(
+							frame_support::sp_runtime::ModuleError {
+								index: 2,
+								error: [0, 0, 0, 0],
+								message: Some("AccounttDoesNotExist"),
+							},
+						));
+					},
+				}
+				// let to_slash = T::Currency::free_balance(&sacked_account) T::Currency::minimum_balance();
+				// match T::Currency::withdraw(&sacked_account, total_issuance, WithdrawReasons::FEE, ExistenceRequirement::KeepAlive) {
+				//     Ok(amount_slashed) => {
+				// 	let (treasury_cut, beneficiary_cut) = amount_slashed.split(T::TreasuryFlatCut::get());
+				// 	to_beneficiary = to_beneficiary.merge(beneficiary_cut);
+				// 	to_treasury = to_treasury.merge(treasury_cut);
+				//     },
+				//     Err(_) => {return Err(DispatchError::Module(frame_support::sp_runtime::ModuleError { index: 2, error: [0, 0, 0, 0], message: Some("AccounttDoesNotExist") })); },
+				// }
+			}
+			T::Currency::deposit_creating(&T::TreasuryAccount::get(), to_treasury.peek());
+			//T::Currency::deposit_creating(&beneficiary, to_beneficiary.peek());
 
+			match T::Currency::deposit_into_existing(&beneficiary, to_beneficiary.peek()) {
+				Ok(_) => Ok(()),
+				Err(_) => Err(DispatchError::Module(
+					frame_support::sp_runtime::ModuleError {
+						index: 2,
+						error: [0, 0, 0, 0],
+						message: Some("AccountDoesNotExist"),
+					},
+				)),
+			}
 			// Todo:
 			// Take as much as possible from each account in `sacked_accounts`,
 			// without removing them from existence
 			// and give it all to beneficiary
 			// except for the TreasuryFlatCut amount, that goes to the treasury for each sacked
 			// account Hint: there is a `split` method implemented on imbalances
-
-			Ok(())
 		}
 	}
 }
