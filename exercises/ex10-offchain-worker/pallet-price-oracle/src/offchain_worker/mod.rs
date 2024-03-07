b091dacdfa4101ba3294e17ad36ee9980a1fad25
M-Bln <mlnd2@protonmail.com>
2024-03-07 18:31:04 +0100

    ex10 start working on offchain worker

--- exercises/ex10-offchain-worker/pallet-price-oracle/src/offchain_worker/mod.rs
+++ exercises/ex10-offchain-worker/pallet-price-oracle/src/offchain_worker/mod.rs
@@ -1,13 +1,18 @@
 mod error;
+use core::str::FromStr;
 
 use error::OffchainWorkerError;
 
 use crate::{Call, Config, Pallet, String, Vec};
 
 use frame_support::sp_runtime::offchain::http;
+use frame_support::log;
+
 
 use serde::Deserialize;
+use serde_json::{Value,Number};
 use sp_arithmetic::{FixedI64, FixedPointNumber};
+use frame_support::sp_io::offchain::{Duration,timestamp};
 
 #[derive(Debug, Deserialize)]
 struct PairBuyPrice {
@@ -26,8 +31,32 @@ pub(crate) fn fetch_btc_price() -> Result<FixedI64, OffchainWorkerError> {
 	// - do an http get request to `"https://api.coinbase.com/v2/prices/BTC-USD/buy`
 	// - extract the price form the response body
 	// - convert it to `FixedI64` before returning it
-
-	Ok(Default::default())
+    let deadline = timestamp().add(Duration::from_millis(2_000));
+    let request = http::Request::get("https://api.coinbase.com/v2/prices/BTC-USD/buy");
+    let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
+    let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
+    if response.code != 200 {
+	log::warn!("Unexpected status code: {}", response.code);
+	return Err(OffchainWorkerError::Request(http::Error::Unknown));
+    }
+    let body = response.body().collect::<Vec<u8>>();
+    let body_json = serde_json::from_slice(&body)?;
+    if let  Some(Value::Number(price)) = body_json.get("amount") {
+	if let Some(price_i64) = price.as_i64() {
+	    return Ok(FixedI64::from(price_i64));
+	} else {
+	   // return Err(OffchainWorkerError::ParsePrice(<f64 as FromStr>::Err));
+	    return Err(OffchainWorkerError::Request(http::Error::Unknown));
+	}
+    } else {
+	return Err(OffchainWorkerError::Request(http::Error::Unknown));
+    }
+    // Create a str slice from the body.
+    // let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
+    // 	log::warn!("No UTF8 body");
+    // 	http::Error::Unknown
+    // })?;
+    // Ok(Default::default())
 }
 
 impl<T: Config> Pallet<T> {
